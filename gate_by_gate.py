18b277014063e9d66944c448aeda347c70850a9e
lara <laraboloni@gmail.com>
2026-05-05 06:01:21 +0200

    tricky dect working and the partial correct resets

--- gate_by_gate.py
+++ gate_by_gate.py
@@ -28,28 +28,24 @@ def generate_labels(n, start_label = None):
         length += 1
     return labels
 
-def _bitstring_to_index(bits: list[int]) -> int:
-    """Convert a bitstring [b_0, b_1, ..., b_{n-1}] to an integer index."""
-    idx = 0
-    for b in bits:
-        idx = (idx << 1) | b
-    return idx
 
 # ---------------------------------------------------------------------------
 # Computing amplitudes
 #  ---------------------------------------------------------------------------
 
 
-def comp_amplitude(val_param: dict[str, Fraction], circ: GraphS, n_qubits: int, ) -> complex:
+def comp_amplitude(val_param: dict[str, Fraction], circs: list[GraphS], n_qubits: int, ) -> complex:
     """
     Compute <x|C|0> by contracting the ZX graph of the circuit postselected on x.
 
     The trick: to get <x|C|0>, we append the bra <x| to the circuit as Z-phase
     gates on the outputs, then contract the whole diagram to a scalar.
     """
+    amplitude = 0
+    for circ in circs:
+        # Extract the scalar using the parameters
+        amplitude += circ.scalar.evaluate_scalar(val_param)
 
-    # Extract the scalar using the parameters
-    amplitude = circ.scalar.evaluate_scalar(val_param)
     return amplitude / (np.sqrt(2) ** n_qubits)
 
 
@@ -57,7 +53,7 @@ def comp_amplitude(val_param: dict[str, Fraction], circ: GraphS, n_qubits: int, 
 # Preprocessing
 #  ---------------------------------------------------------------------------
 
-def split_circuit_reduce(circ_until_now: tsim.Circuit, y: list[int], num_qubits: int) -> list[GraphS]:
+def split_circuit_reduce(circ_until_now: tsim.Circuit, y: list[int], num_qubits: int, reset_list: list):
     """
     Compute <x|C|0> by contracting the ZX graph of the circuit postselected on x.
 
@@ -69,6 +65,12 @@ def split_circuit_reduce(circ_until_now: tsim.Circuit, y: list[int], num_qubits:
     last_vertices  = {}
     for v in g.vertices():
         q = g.qubit(v)
+        param_val = g.get_params(v)
+        if param_val in reset_list:
+            for neighbor in g.neighbors(v):
+                if "rec" in str(g.get_params(neighbor)):
+                    new_sym = str(g.get_params(neighbor)).replace("{", "").replace("}", "").replace("'", "")
+                    g.set_phase(v, new_sym)
         if q not in last_vertices or g.row(v) > g.row(last_vertices[q]):
             last_vertices[q] = v
 
@@ -82,17 +84,17 @@ def split_circuit_reduce(circ_until_now: tsim.Circuit, y: list[int], num_qubits:
             g.set_type(out_vertex, zx.VertexType.X)
             g.add_params(out_vertex, y[qubit])
 
-    # Simplify with ParamZX's parameterised reduction
+    # Simplify with ParamZX's parameterised reduction and stab decomp
     zx.full_reduce(g, paramSafe=True)
-
-    return g
-
+    gs = zx.simulate.find_stabilizer_decomp(g)
+    return gs
 
 def preprocessing(circuit: tsim.Circuit) :
     circ_until_now = tsim.Circuit()
     num_qubits = circuit.num_qubits
     y = generate_labels(num_qubits)
-    # circ_until_now.append_from_stim_program_text(f"R {' '.join(str(q) for q in range(num_qubits))}")
+    reset_list = [{f"m[{i}]"} for i in range(40)]
+    is_initialized = [False] * num_qubits
     all_sub_circuits = []
 
     num_gates = len(circuit)
@@ -110,7 +112,7 @@ def preprocessing(circuit: tsim.Circuit) :
                 q = targets[i].qubit_value
                 circ_until_now.append_from_stim_program_text(f"H {q}")
 
-                all_sub_circuits.append(split_circuit_reduce(circ_until_now, y, num_qubits))
+                all_sub_circuits.append(split_circuit_reduce(circ_until_now, y, num_qubits, reset_list))
 
             elif gate.name == "Z":
                 q = targets[i].qubit_value
@@ -125,7 +127,7 @@ def preprocessing(circuit: tsim.Circuit) :
 
             elif gate.name == "S_DAG":
                 q = targets[i].qubit_value
-                if gate.tag == "T_DAG":
+                if gate.tag == "T":
                     circ_until_now.append_from_stim_program_text(f"T_DAG {q}")
                 else:
                     circ_until_now.append_from_stim_program_text(f"S_DAG {q}")
@@ -137,10 +139,18 @@ def preprocessing(circuit: tsim.Circuit) :
             elif gate.name == "R":
                 q = targets[i].qubit_value
                 circ_until_now.append_from_stim_program_text(f"R {q}")
+                is_initialized[q] = True
 
             elif gate.name == "RX":
                 q = targets[i].qubit_value
-                circ_until_now.append_from_stim_program_text(f"RX {q}")
+                if not is_initialized[q]:
+                    circ_until_now.append_from_stim_program_text(f"RX {q}")
+                    is_initialized[q] = True
+                else:
+                #     circ_until_now.append_from_stim_program_text(f"H {q}")
+                #     all_sub_circuits.append(split_circuit_reduce(circ_until_now, y, num_qubits, reset_list))
+                    circ_until_now.append_from_stim_program_text(f"RX {q}")
+                    # circ_until_now.append_from_stim_program_text(f"X {q}")
 
             elif gate.name == "M":
                 q = targets[i].qubit_value
@@ -151,14 +161,14 @@ def preprocessing(circuit: tsim.Circuit) :
 
                 # HADAMARD
                 circ_until_now.append_from_stim_program_text(f"H {q}")
-                all_sub_circuits.append(split_circuit_reduce(circ_until_now, y, num_qubits))
+                all_sub_circuits.append(split_circuit_reduce(circ_until_now, y, num_qubits, reset_list))
 
                 # Measure
                 circ_until_now.append_from_stim_program_text(f"M {q}")
-                #
+
                 # # HADAMARD
                 # circ_until_now.append_from_stim_program_text(f"H {q}")
-                # all_sub_circuits.append(split_circuit_reduce(circ_until_now, y, num_qubits))
+                # all_sub_circuits.append(split_circuit_reduce(circ_until_now, y, num_qubits, reset_list))
 
             elif gate.name == "X_ERROR":
                 q = targets[i].qubit_value
@@ -187,18 +197,17 @@ def preprocessing(circuit: tsim.Circuit) :
 # Algorithm for gate by gate
 # ---------------------------------------------------------------------------
 
-def gate_by_gate(circuit: tsim.Circuit, split_circs: list[GraphS], detectors: list = None):
+def gate_by_gate(circuit: tsim.Circuit, split_circs: list[list[GraphS]], detectors: list = None):
     circ_until_now = tsim.Circuit()
     num_qubits = circuit.num_qubits
     y = [0] * num_qubits
     all_dicts = AllDictionaries(num_qubits)
     strings = generate_labels(num_qubits)
-
+    is_initialized = [False] * num_qubits
     # dictionary = dict(zip(strings, y))
     # circ_until_now.append_from_stim_program_text(f"R {' '.join(str(q) for q in range(num_qubits))}")
     new_detectors = []
     num_Had = 0
-
     num_gates = len(circuit)
     for gate in circuit:
 
@@ -212,114 +221,50 @@ def gate_by_gate(circuit: tsim.Circuit, split_circs: list[GraphS], detectors: li
                 circ_until_now.append_from_stim_program_text(f"CX {a} {b}")
 
             if gate.name == "H":
-                q = targets[i].qubit_value
-                circ_until_now.append_from_stim_program_text(f"H {q}")
-                rec = dict(zip(all_dicts.rec_list, all_dicts.measurement_rec))
-                m = dict(zip(all_dicts.reset_list, all_dicts.measurement_rec))
-
-                #make two diverging copies of y
-                val0 = create_y(0, y, q, strings)
-                val1 = create_y(1, y, q, strings)
-
-                z0 = comp_amplitude(val0 | rec | m, split_circs[num_Had], num_qubits)
-                z1 = comp_amplitude(val1 | rec | m, split_circs[num_Had], num_qubits)
-
+                y = perform_had(all_dicts, split_circs[num_Had], num_qubits, y, targets[i].qubit_value)
                 num_Had += 1
-
-                #calculate probability
-                denom = abs(z0) ** 2 + abs(z1) ** 2
-                p = abs(z0) ** 2 / denom
-
-                # Sample y_q
-                y[q] = 0 if random.random() < p else 1
-
-            if gate.name == "Z":
-                q = targets[i].qubit_value
-                circ_until_now.append_from_stim_program_text(f"Z {q}")
-
-            if gate.name == "S":
-                q = targets[i].qubit_value
-                if gate.tag == "T":
-                    circ_until_now.append_from_stim_program_text(f"T {q}")
-                else:
-                    circ_until_now.append_from_stim_program_text(f"S {q}")
-
-            if gate.name == "S_DAG":
-                q = targets[i].qubit_value
-                if gate.tag == "T_DAG":
-                    circ_until_now.append_from_stim_program_text(f"T_DAG {q}")
-                else:
-                    circ_until_now.append_from_stim_program_text(f"S_DAG {q}")
 
             if gate.name == "X":   #Dont split
                 q = targets[i].qubit_value
-                circ_until_now.append_from_stim_program_text(f"X {q}")
-
                 y[q] ^= 1
 
             if gate.name == "R":
                 q = targets[i].qubit_value
                 y[q] = 0
-                circ_until_now.append_from_stim_program_text(f"R {q}")
+                is_initialized[q] = True
 
             if gate.name == "RX":
+
                 q = targets[i].qubit_value
+                # HADAMARD
+                if not is_initialized[q]:
+                    y[q] = 0 if random.random() < 0.5 else 1
+                    is_initialized[q] = True
+                else:
+                    # y = perform_had(all_dicts, split_circs[num_Had], num_qubits, y, targets[i].qubit_value)
+                    # num_Had += 1
 
-                circ_until_now.append_from_stim_program_text(f"R {q}")
+                    # # --- Initialize into |+> --- # always 50/50 so just sample directly
+                    y[q] = 0 if random.random() < 0.5 else 1
 
-                # # --- Initialze into |+> --- # always 50/50 so just sample directly
-                y[q] = 0 if random.random() < 0.5 else 1
-
-                circ_until_now.append_from_stim_program_text(f"RX {q}")
 
             if gate.name == "M":
                 q = targets[i].qubit_value
                 all_dicts.measurement_rec.append(y[q])
-                circ_until_now.append_from_stim_program_text(f"M {q}")
 
             if gate.name == "MX":
-                q = targets[i].qubit_value
+                # print("num had:" + str(num_Had))
+                # print(y)
 
                 # HADAMARD
-                circ_until_now.append_from_stim_program_text(f"H {q}")
-                rec = dict(zip(all_dicts.rec_list, all_dicts.measurement_rec))
-                m = dict(zip(all_dicts.reset_list, all_dicts.measurement_rec))
-
-                # make two diverging copies of y
-                val0 = create_y(0, y, q, strings)
-                val1 = create_y(1, y, q, strings)
-
-                z0 = comp_amplitude(val0 | rec | m, split_circs[num_Had], num_qubits)
-                z1 = comp_amplitude(val1 | rec | m, split_circs[num_Had], num_qubits)
-
-                denom = abs(z0) ** 2 + abs(z1) ** 2
-                p = abs(z0) ** 2 / denom
-
-                y[q] = 0 if random.random() < p else 1
-
+                y = perform_had(all_dicts, split_circs[num_Had], num_qubits, y, targets[i].qubit_value)
                 num_Had += 1
 
                 # Measure
                 all_dicts.measurement_rec.append(y[q])
-                circ_until_now.append_from_stim_program_text(f"M {q}")
 
-                # #HADAMARD
-                # circ_until_now.append_from_stim_program_text(f"H {q}")
-                # rec = dict(zip(all_dicts.rec_list, all_dicts.measurement_rec))
-                # m = dict(zip(all_dicts.reset_list, all_dicts.reset_vals))
-                #
-                # # make two diverging copies of y
-                # val0 = create_y(0, y, q, strings)
-                # val1 = create_y(1, y, q, strings)
-                #
-                # z0 = comp_amplitude(val0 | rec | m, split_circs[num_Had], num_qubits)
-                # z1 = comp_amplitude(val1 | rec | m, split_circs[num_Had], num_qubits)
-                #
-                # denom = abs(z0) ** 2 + abs(z1) ** 2
-                # p = abs(z0) ** 2 / denom
-                #
-                # y[q] = 0 if random.random() < p else 1
-                #
+                # # HADAMARD
+                # y = perform_had(all_dicts, split_circs[num_Had], num_qubits, y, targets[i].qubit_value)
                 # num_Had += 1
 
             if gate.name == "X_ERROR":
@@ -328,26 +273,21 @@ def gate_by_gate(circuit: tsim.Circuit, split_circs: list[GraphS], detectors: li
 
                 if random.random() < prob[0]:
                     y[q] ^= 1
-                circ_until_now.append_from_stim_program_text(f"X_ERROR({prob[0]}) {q}")
 
             if gate.name == "Z_ERROR":
                 q = targets[i].qubit_value
                 prob = gate.gate_args_copy()
-                circ_until_now.append_from_stim_program_text(f"Z_ERROR({prob[0]}) {q}")
 
         if gate.name == "DETECTOR":
             result = 0
             for i in targets:
                 result ^= all_dicts.measurement_rec[i.value]
-                print(all_dicts.measurement_rec[i.value], end=", ")
 
             new_detectors.append(result)
             if detectors is not None:
-                print(f"y = {y}", end=" ")
                 if detectors[len(new_detectors) - 1] != new_detectors[-1]:
-                    print("no")
+                    print("not passed")
                     return False, None, detectors
-            print("passed")
 
 
     if detectors is None:
@@ -356,23 +296,24 @@ def gate_by_gate(circuit: tsim.Circuit, split_circs: list[GraphS], detectors: li
     return True, y, detectors
 
 
-# def perform_had():
-#     circ_until_now.append_from_stim_program_text(f"H {q}")
-#     rec = dict(zip(rec_list, measurement_rec))
-#     m = dict(zip(reset_list, reset_vals))
-#
-#     val0 = create_y(0, y, q, strings)
-#     val1 = create_y(1, y, q, strings)
-#
-#     z0 = comp_amplitude(val0 | rec | m, split_circs[num_Had], num_qubits)
-#     z1 = comp_amplitude(val1 | rec | m, split_circs[num_Had], num_qubits)
-#
-#     denom = abs(z0) ** 2 + abs(z1) ** 2
-#     p = abs(z0) ** 2 / denom
-#
-#     y[q] = 0 if random.random() < p else 1
+def perform_had(dics, split_circ: list[GraphS], num_qubits, y, q):
+    rec = dict(zip(dics.rec_list, dics.measurement_rec))
+    m = dict(zip(dics.reset_list, dics.reset_vals))
 
+    val0 = create_y(0, y, q, dics.strings)
+    val1 = create_y(1, y, q, dics.strings)
 
+    z0 = comp_amplitude(val0 | rec | m, split_circ, num_qubits)
+    z1 = comp_amplitude(val1 | rec | m, split_circ, num_qubits)
+
+    denom = abs(z0) ** 2 + abs(z1) ** 2
+    if denom == 0:
+        denom = 1
+    p = abs(z0) ** 2 / denom
+
+    y[q] = 0 if random.random() < p else 1
+
+    return y
 
 
 
